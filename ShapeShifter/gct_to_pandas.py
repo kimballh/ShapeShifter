881458c404967ff280f36fb9e9889b798c444472
Louisa Dayton <dayton.louisa@gmail.com>
2018-06-25 15:26:03 -0600

    Should be a bit faster

--- ShapeShifter/gct_to_pandas.py
+++ ShapeShifter/gct_to_pandas.py
@@ -1,24 +1,4 @@
 import pandas as pd
-
-#Takes a GCT file and reads it into a pandas dataframe
 def gctToPandas(fileName):
-    col = []
-    data = []
-    with open(fileName) as readFile: 
-        i = 0
-        for line in readFile:
-            line = line.rstrip("\n")
-            i += 1
-#Skip the first two lines
-            if i <= 2:
-                continue
-#Read the column names into a list
-            if i == 3:
-                col = line.split("\t")
-#Read the rest of the data into a list of lists
-            else:
-                data.append(line.split("\t"))
-    df = pd.DataFrame(data, columns=col)
-    return df 
-
-
+    df = pd.read_csv(filepath_or_buffer="allaml.dataset.gct", sep='\t', skiprows=2)
+    return df

ed81c218c5b907db3b410b405ef5b5d0a8fcf493
Louisa Dayton <dayton.louisa@gmail.com>
2018-07-05 14:23:12 -0600

    Will give the correct dtype unless column includes a NaN value

--- ShapeShifter/arff_to_pandas.py
+++ ShapeShifter/arff_to_pandas.py
@@ -4,35 +4,33 @@ import numpy as np
 
 #Converts an ARFF file (the parameter) into a pandas dataframe
 def arffToPandas(fileName):
+    i = 0
     columnNames = []
     dataList = []
     pattern = r"@[Aa][Tt][Tt][Rr][Ii][Bb][Uu][Tt][Ee]\s+([^\s]*)\s+.*"
-   
-  
     with open(fileName) as dataFile:
-        for line in dataFile: 
+        for line in dataFile:
+            print(line) 
+            i += 1 
             line = line.rstrip("\n")
 #Takes the column names in the attribute and put them in a list 
             if line.startswith("@ATTRIBUTE") or line.startswith("@attribute"):  
                 columnNames.append(re.sub(pattern, r"\1", line)) 
                 continue
+            elif line.startswith("@DATA") or line.startswith("@data"):
+                break
             elif line.startswith("@") or line.startswith("%") or line == "":
                 continue
-#Turns the lines into lists and put them into a list 
-            dataList.append(line.split(","))      
-    data = pd.DataFrame(dataList, columns = columnNames)
+              
+#Turns the lines into lists and put them into a list
+
+    data = pd.read_csv(filepath_or_buffer=fileName, sep=',', names=columnNames, skiprows=i)
+
     data = data.replace("?", np.nan)
-    for column in data:
-#Finds columns with numbers and changes the datatype to int or float
-        data[column] = pd.to_numeric(data[column], errors='ignore')
-#Converts true/false strings into booleans
-        for cell in data[column]:
-            if str(cell).lower() == "true":
-                data[column] = data[column].replace(cell, True)
-            elif str(cell).lower() == "false":
-                data[column] = data[column].replace(cell, False)
+
     return data 
-    
+   
+
 
 
 

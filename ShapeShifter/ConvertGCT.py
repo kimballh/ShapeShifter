379b50913ef3da6529531bc7e0e4eb84ba632310
Louisa Dayton <dayton.louisa@gmail.com>
2018-08-08 16:19:22 -0600

    Hopefully GCT works now

--- ShapeShifter/ConvertGCT.py
+++ ShapeShifter/ConvertGCT.py
@@ -3,16 +3,15 @@ import pandas as pd
 #Takes a pandas dataframe and converts it to a file
 #The file will be named after the second parameter
 def toGCT(df, fileName):
-    df = df.fillna("")
+    #df = df.fillna("")
 #Write #1.2 (the version string)
     writeFile = open(fileName, 'w')
     writeFile.write("#1.2\n")
 #Write the number of rows and the number of columns
     writeFile.write(str(len(df.index)))
     writeFile.write("\t" + str(len(df.columns[2:])) + "\n")
-#Write the data
-    writeFile.write(str(df))
-    writeFile.close()
+#Write the dataframe
+    df.to_csv(path_or_buf=fileName, sep="\t", na_rep="", mode='a')
 
 #Takes a GCT file and reads it into a pandas dataframe
 def gctToPandas(fileName):
